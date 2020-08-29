880ff171f208e331dc335828d1d7dbefa0522752
Rainer Brieger <hello@rainerb.com>
2020-08-29 10:07:52 -0600

    Added code to html page, added players, images, grid. Added code to JS with cookies set

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Pikachu Function
+function pikachuClick() {
+  var playerPikachu = document.getElementById("pikachu");
+  var windowNew = window.open("battle.html", "_self");
+  Cookies.set("pokemon", "Pikachu");
+}
+
+// Jigglypuff Function
+function jigglypuffClick() {
+  var playerJigglypuff = document.getElementById("jigglypuff");
+  var windowNew = window.open("battle.html", "_self");
+  Cookies.set("pokemon", "Jigglypuff");
+}
+
+// Charmander Function
+function charmanderClick() {
+  var playerCharmander = document.getElementById("peaktime");
+  var windowNew = window.open("battle.html", "_self");
+  Cookies.set("pokemon", "Charmander");
+}
